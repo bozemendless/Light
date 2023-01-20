2edb1a2b08fe2ad9531899503ca5e3ea564eeaf1
bozemendless <98375601+bozemendless@users.noreply.github.com>
2023-01-20 17:44:06 +0800

    feat: add init files of new app-- chats

---
+++ light/chats/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.utils import timezone
+
+class Chats(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    content = models.TextField(max_length=255)
+    image = models.ImageField(upload_to='images/')
+    create_time = models.DateTimeField(default=timezone.now)
