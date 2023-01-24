a3810f1811206dda5e019ec959c129ed0ff42df2
bozemendless <98375601+bozemendless@users.noreply.github.com>
2023-01-25 02:31:24 +0800

    feat: add favicon.ico

--- light/light/urls.py
+++ light/light/urls.py
@@ -1,5 +1,4 @@
-"""light URL Configuration
-"""
+"""light URL Configuration"""
 
 from django.contrib import admin
 from django.urls import include, path
@@ -8,7 +7,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('channels/', include('channels.urls')),
-    # path('admin/', admin.site.urls),
+    path('', include('users.urls')),
+    path('admin/', admin.site.urls),
 ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += path('favicon.ico', include('static/light/')),
