32557b9efe648488d9eaa32544f1187e8a2733eb
bozemendless <98375601+bozemendless@users.noreply.github.com>
2023-01-25 02:32:19 +0800

    feat: update global setting

--- light/light/settings.py
+++ light/light/settings.py
@@ -39,6 +39,8 @@ CSRF_TRUSTED_ORIGINS = [
     'https://*.shichenx.com'
 ]
 
+# Auth custom model
+AUTH_USER_MODEL = 'users.User'
 
 # Application definition
 
@@ -91,13 +93,19 @@ WSGI_APPLICATION = 'light.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': database,
+        'USER': user,
+        'PASSWORD': password,
+        'HOST': host,
+        'PORT': port,
+        'OPTIONS': {
+            'init_command': "SET time_zone='+8:00'",
+        },
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -134,6 +142,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
