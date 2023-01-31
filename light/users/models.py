6b6c85bc3da8582a5785ea14c2deaaa2c311803c
bozemendless <98375601+bozemendless@users.noreply.github.com>
2023-01-31 21:11:40 +0800

    feat: complete register function with fields verification at both the front and back ends, and it will also confirm if the e-mail address and the username are duplicated

--- light/users/models.py
+++ light/users/models.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     id = models.AutoField(primary_key=True)
-    email = models.EmailField(max_length=255, unique=True)
-    password = models.CharField(max_length=255)
+    email = models.EmailField(max_length=64, unique=True)
+    password = models.CharField(max_length=72)
--- light/users/views.py
+++ light/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import User
 import json
+import re
 
 def login_page(request):
     return render(request, 'users/login.html')  
@@ -29,19 +30,68 @@ def login(request):
 
 def register(request):
 
-    if request.method == "POST":
-        user = User()
+    if request.method == 'POST':
 
-        request_body = json.loads(request.body)
-        
-        user.email = request_body['email']
-        user.username = request_body['username']
-        user.password = request_body['password']
+        try:
+            # Parse the form
+            infos = json.loads(request.POST.get('infos'))
+            email = infos['email']
+            username = infos['username']
+            password = infos['password']
 
-        print(user) # default username
+            # Verify the register form
+            email_regex = r'^(?=.{8,64}$)\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$'
+            username_regex = r'^[A-Za-z0-9]{2,32}$'
+            password_regex = r'.{6,72}$'
 
-        res = {
-            'ok': True
-        }
+            is_email_valid = re.search(email_regex, email)
+            is_username_valid = re.search(username_regex, username)
+            is_password_valid = re.search(password_regex, password)
 
-        return JsonResponse(res)
+            if not(is_email_valid and is_username_valid and is_password_valid):
+                error = "the fields are not valid"
+                res = {
+                    'error': True,
+                    'message': error
+                }
+                return JsonResponse(res, status=400)
+
+            # Check if account exist or field not unique
+            is_email_exists = User.objects.filter(email=email).exists()
+            is_username_exists = User.objects.filter(username=username).exists()
+
+            if is_email_exists or is_username_exists:
+                if is_email_exists:
+                    error = 'email already exists'
+                if is_username_exists:
+                    error = 'username already exists'
+                if is_email_exists and is_username_exists:
+                    error = 'email and username already exist'
+                res = {
+                        'error': True,
+                        'message': error
+                    }
+                return JsonResponse(res, status=400)
+
+            # New account register
+            user = User()
+
+            user.email = infos['email']
+            user.username = infos['username']
+            user.password = infos['password']
+
+            user.save()
+
+            res = {
+                'ok': False
+            }
+
+            return JsonResponse(res)
+
+        except:
+            error = 'Internal Server Error'
+            res = {
+                'error':True,
+                'message': error
+            }
+            return JsonResponse(res, status=500)
