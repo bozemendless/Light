d5dc3e4ebb8b5708e261ce987a9cec575cdbbf29
bozemendless <98375601+bozemendless@users.noreply.github.com>
2023-02-28 23:04:00 +0800

    feat: add create server API.

--- light/channel/urls.py
+++ light/channel/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.channel, name='channel'),
+    path('api/server', views.server),
+    path('api/server/add_member', views.server_add_member),
 ] 
--- light/channel/views.py
+++ light/channel/views.py
@@ -1,6 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from accounts.views import check_login
+from django.http import JsonResponse
+import json
+import jwt
+from dotenv import load_dotenv
+import os
+from . models import Server
+from accounts.models import Account
+
+load_dotenv()
+jwt_secret_key = os.getenv('JWT_SECRET_KEY')
 
 @check_login
 def channel(request):
     return render(request, 'channel/channel.html')
+
+def server(request):
+    if request.method == 'GET':
+        pass
+    if request.method == 'POST':
+        # verify the token
+        if 'token' in request.session:
+            try:
+                user_token = request.session['token']
+                decode_token = jwt.decode(
+                user_token, jwt_secret_key, algorithms="HS256")
+                decode_user_id = decode_token['id']
+            except:
+                del request.session['token']
+                if request.path != '/login':
+                    return redirect('/login')
+
+            try:
+                account = Account.objects.get(id=decode_user_id)
+                infos = json.loads(request.body)
+                name = infos['name']
+                server = Server(name=name, creator=account)
+                server.save()
+                server.members.add(account)
+                print(server)
+                res = {
+                    'ok': True,
+                }
+                return JsonResponse(res)
+
+            except:
+                error = 'Failed to create server'
+                res = {
+                    'error': True,
+                    'message': error,
+                }
+                return JsonResponse(res, 400)
+        else: 
+            # token not in session
+            return redirect('/login')
+        
+
+
+def server_add_member(request):
+    pass
