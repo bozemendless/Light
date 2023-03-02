9921f8bf1449b69539d7dba955eb6521df0e0aea
bozemendless <98375601+bozemendless@users.noreply.github.com>
2023-03-03 04:24:42 +0800

    feat: change the path of 'api/server/add_members' into RESTful style - 'api/server/members'.

--- light/channel/urls.py
+++ light/channel/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.channel, name='channel'),
     path('api/server', views.server),
-    path('api/server/add_member', views.server_add_member),
+    path('api/server/members', views.server_members),
 ] 
