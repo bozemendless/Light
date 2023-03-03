778b14ef4252b05da07a26649f5f6079112dc7f1
bozemendless <98375601+bozemendless@users.noreply.github.com>
2023-03-04 02:45:34 +0800

    feat: add a new API for user save their about_me content.

--- light/accounts/urls.py
+++ light/accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('api/user', views.register),
     path('api/user/auth',views.auth),
     path('api/user/avatar', views.avatar),
+    path('api/user/about_me', views.about_me),
 ] 
